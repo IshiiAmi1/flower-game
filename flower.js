a532b4a89f23d716ff3165875c2f86061547f7ea
IshiiAmi1 <s1270024@u-aizu.ac.jp>
2020-09-21 17:56:43 +0900

    “17”

--- flower.js
+++ flower.js
@@ -27,6 +27,7 @@ function preload(){
   image3=loadImage('image/fubuki.png');
   image4=loadImage('image/opening.JPG');
   image5=loadImage('image/background.png');
+  image6=loadImage('image/ending.JPG');
 }
 
 function move(){
@@ -58,8 +59,10 @@ function move(){
 }
 
 function drawResultScreen() {
-  background(0, 192); // 透明度 192 の黒
-  fill(255);
+//background(0, 192); // 透明度 192 の黒
+     image(image6,0,0,375,667);
+    background(0, 130);
+  fill(250);
   textSize(30);
   textAlign(CENTER, CENTER); // 横に中央揃え ＆ 縦にも中央揃え
   text("RESULT", width / 2, height / 2); // 画面中央にテキスト表示
@@ -67,26 +70,27 @@ function drawResultScreen() {
   //result=((30*30)*flowers.length)/(canvasx*canvasy)*100;    
     result=(flowers.length/SEED)*100;
     textSize(35);
-  text("咲いた花の数"+flowers.length+"個",width/2,height/2+100);
+  text("咲いた桜の数"+flowers.length+"個",width/2,height/2+100);
     acievement();
   if(mouseIsPressed==true){
       truecount++;
-      if(truecount==20){
+      if(truecount==30){
           gameState="reset";  
       }
   }
 }
 
 function acievement(){
+   
     textSize(20);
-    if(flowers.length>=150){
-        text("天才！？藤の花がすごく沢山咲いた！",width/2,height/2+150);
-    }else if(flowers.length<150 && flowers.length>=130){
-         text("おめでとう！藤の花が沢山咲いた！",width/2,height/2+150);
+    if(flowers.length>=135){
+        text("天才！？桜がすごく沢山咲いた！",width/2,height/2+150);
+    }else if(flowers.length<135 && flowers.length>=130){
+         text("おめでとう！桜が沢山咲いた！",width/2,height/2+150);
     }else if(flowers.length<130 && flowers.length>=100){
-         text("まあまあの藤の花が咲いた",width/2,height/2+150);
+         text("まあまあの桜が咲いた",width/2,height/2+150);
     }else if(flowers.length<100 && flowers.length>=70){
-         text("んーーーー、藤の花がちょっと咲いた...",width/2,height/2+150);
+         text("んーーーー、桜がちょっと咲いた...",width/2,height/2+150);
     }else{
          text("もう少し頑張って下さい...",width/2,height/2+150);
     }
@@ -95,6 +99,7 @@ function acievement(){
 function startScreen(){
     image(image4,0,0,375,667);
    // background(255,184, 226,80);
+     background(0, 60);
     fill(255);  
     textAlign(CENTER, CENTER); // 横に中央揃え ＆ 縦にも中央揃え
     if(mouseIsPressed==true){
@@ -102,14 +107,14 @@ function startScreen(){
     }
     if(state==0){
           textSize(30);
-          text("〜戸赤のやまざくらゲーム〜", width / 2, height / 4); // 画面中央にテキスト表示
+          text("〜戸赤の山桜ゲーム〜", width / 2, height / 4); // 画面中央にテキスト表示
         textSize(20);
-        text("たくさんの花を咲かせて下さい!\n\n\n\n✳︎音が出ます音量注意",width / 2, height / 2);
+        text("たくさん桜を咲かせて下さい!\n\n\n\n✳︎音が出ます音量注意",width / 2, height / 2);
     }else if(state<=20){
-        fill(255,100,255)
+        fill(255)
         text("~操作方法~",width / 2, height / 5);
         fill(255);  
-        text("Tap している間種がまかれています。\nおじいさんの動きに合わせて種をまいて\nより多くの花を山に咲かせてください\n種の数や秒数が決まっているので\n頑張って下さい！",width / 2, height / 3+90);
+        text("Tap している間種がまかれています。\nおじいさんの動きに合わせて種をまいて\nより多くの桜を山に咲かせてください\n種の数や秒数が決まっているので\n頑張って下さい！",width / 2, height / 3+90);
     }else{
         resetgame();
     }
@@ -129,8 +134,8 @@ function resetgame(){
   vy=0;
   seeds=[];
   flowers=[];
-    SEED=300;
-  SEEDNUM=300;
+    SEED=250;
+  SEEDNUM=250;
 
     var i, j;
     imagearray = new Array(Math.round(canvasx/30));
@@ -182,13 +187,18 @@ function playgame(){
     //text("flowernum:"+flowers.length,101,50);
     move();
     if(mouseIsPressed==true){
+        
         seedtimer++;
         if(seedtimer==5 && SEEDNUM>0){
             SEEDNUM--;
             seeds.push(new seed(vx,vy));
             seedtimer=0;
         }
-        //pushtimer++;
+        pushtimer++;
+        if(pushtimer==20){
+           // music.play();
+            pushtimer=0;
+        }
         //fill(0,0,255);
      }
     
@@ -203,9 +213,7 @@ function playgame(){
     }
 }
 
-function audio(){
-    
-}
+
 
 function setup() {
   canvasx=375;
@@ -213,10 +221,7 @@ function setup() {
 　//image(image5,50,500,375,667);
   createCanvas(canvasx, canvasy);
   gameState="start";
-  //imagearray=[];
-    
- // startScreen();
- // resetgame();
+ 
 }
 
 function draw() {
