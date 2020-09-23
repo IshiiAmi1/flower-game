f9c8ca4fd839ef4eee3c3b7985adb00d33d2e402
IshiiAmi1 <s1270024@u-aizu.ac.jp>
2020-09-23 15:34:25 +0900

    “29”

--- flower.js
+++ flower.js
@@ -68,7 +68,7 @@ function drawResultScreen() {
   text("RESULT", width / 2, height / 2); // 画面中央にテキスト表示
   text("Tap to restart",width / 2, height / 4);
    textSize(15); 
- text("イラスト　いらすとや　様\n音楽    魔王魂   　様",canvasx-100,canvasy-30);
+ text("桜・灰の絵　いらすとや　様\n音楽　    魔王魂   　様",canvasx-100,canvasy-30);
   //result=((30*30)*flowers.length)/(canvasx*canvasy)*100;    
     result=(flowers.length/SEED)*100;
     textSize(35);
